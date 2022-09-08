7ebe926d4de1f34d3f684d19d177c9e99291a51f
3m1n3nc3 <mygames.ng@gmail.com>
2022-09-08 09:19:07 +0100

    Update Migration

--- database/migrations/2022_09_05_064716_update_companies_table.php
+++ database/migrations/2022_09_05_064716_update_companies_table.php
@@ -17,12 +17,20 @@ return new class extends Migration
         {
             $sts = implode("', '", ['unverified', 'pending', 'verifying', 'verified']);
             DB::statement("ALTER TABLE `companies` CHANGE `status` `status` ENUM('$sts') NOT NULL DEFAULT 'unverified';");
+        } else {
+            Schema::table('companies', function (Blueprint $table) {
+                $table->enum('status', ['unverified', 'pending', 'verifying', 'verified'])->after('logo')->default('unverified');
+            });
         }
 
         if (Schema::hasColumn('companies', 'type'))
         {
             $tps = implode("', '", ['vendor', 'provider']);
             DB::statement("ALTER TABLE `companies` CHANGE `type` `type` ENUM('$tps') NOT NULL DEFAULT 'provider';");
+        } else {
+            Schema::table('companies', function (Blueprint $table) {
+                $table->enum('type', ['vendor', 'provider'])->after('name')->default('provider');
+            });
         }
 
         Schema::table('companies', function (Blueprint $table) {

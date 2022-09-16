9f83426966ceac1c5e1e915b886a11722691c844
3m1n3nc3 <mygames.ng@gmail.com>
2022-09-16 12:54:39 +0100

    Add run queues command to web user

--- app/Http/Controllers/WebUser.php
+++ app/Http/Controllers/WebUser.php
@@ -17,6 +17,7 @@ class WebUser extends Controller
     protected $commands = [
         ['warn' => false, 'command' => 'artisan/list', 'label' => 'Help and Info'],
         ['warn' => false, 'command' => 'artisan/storage:link', 'label' => 'Sym Link Storage'],
+        ['warn' => false, 'command' => 'queue:work', 'label' => 'Run Queues'],
         ['warn' => false, 'command' => 'artisan/migrate', 'label' => 'Migrate Database'],
         ['warn' => true, 'command' => 'artisan/db:seed', 'label' => 'Seed Database'],
         ['warn' => false, 'command' => 'artisan/db:seed HomeDataSeeder', 'label' => 'Seed Homepage'],

b7f2e2eafbebd78a01eb499027662ee0dcfaa6c0
3m1n3nc3 <mygames.ng@gmail.com>
2022-11-10 13:21:29 +0100

    Debugging 8

--- app/Http/Controllers/Api/v1/Provider/OrderRequestController.php
+++ app/Http/Controllers/Api/v1/Provider/OrderRequestController.php
@@ -103,7 +103,7 @@ class OrderRequestController extends Controller
             ])->filter(fn ($i) => (bool) $i)->implode(', ')
         ) : Auth::user()->address;
         $order_request->due_date = $request->due_date;
-        // $order_request->user->notify(new NewServiceOrderRequest($order_request));
+        $order_request->user->notify(new NewServiceOrderRequest($order_request));
         // $order_request->company->notify(new NewServiceOrderRequest($order_request));
         // dd($service);
         // dd($service);

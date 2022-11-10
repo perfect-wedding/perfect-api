f79f591c04a013732953f6f2540f0fbe8492664c
3m1n3nc3 <mygames.ng@gmail.com>
2022-11-10 14:59:19 +0100

    Debugging 18

--- app/Http/Controllers/Api/v1/Provider/OrderRequestController.php
+++ app/Http/Controllers/Api/v1/Provider/OrderRequestController.php
@@ -106,7 +106,7 @@ class OrderRequestController extends Controller
         // $order_request->user->notify(new NewServiceOrderRequest($order_request));
         // $order_request->company->notify(new NewServiceOrderRequest($order_request));
 
-        // $order_request->save();
+        $order_request->save();
         // dd($order_request);
 
         return (new OrderRequestResource($order_request))->additional([

678f5f08cb384d806e4f57a77defb5f524d405c0
3m1n3nc3 <mygames.ng@gmail.com>
2022-11-23 07:01:02 +0100

    Update migration

--- database/migrations/2022_08_18_183644_create_pages_table.php
+++ database/migrations/2022_08_18_183644_create_pages_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('homepages', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
-            $table->string('details')->nullable()->fulltext();
+            $table->longText('details')->nullable()->fulltext();
             $table->string('banner')->nullable();
             $table->string('meta')->fullText()->nullable();
             $table->string('slug')->nullable();

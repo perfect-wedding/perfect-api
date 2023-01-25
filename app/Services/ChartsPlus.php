de245fae520d016ff0e10e15a85a0f18380ea180
3m1n3nc3 <mygames.ng@gmail.com>
2023-01-25 21:25:25 +0100

    Fixed: Incorrect range for charts (Move to end of mont comparison)

--- app/Services/ChartsPlus.php
+++ app/Services/ChartsPlus.php
@@ -26,13 +26,13 @@ class ChartsPlus
     {
         // Add the last 6 months to the dimensions array
         $orders = Trend::query(Order::completed()->byCompany($company->id))
-            ->between(now()->startOfMonth()->subMonths(6), now())
+            ->between(now()->endOfMonth()->subMonths(6), now())
             ->perMonth()->sum('amount')->mapWithKeys(function ($item) {
                 return [$item->date => $item->aggregate];
             });
 
         // $transactions = Trend::query(Transaction::status('completed')->belongsToCompany($company->id))
-        //     ->between(now()->startOfMonth()->subMonths(6), now())
+        //     ->between(now()->endOfMonth()->subMonths(6), now())
         //     ->perMonth()->sum('amount')->mapWithKeys(function ($item) {
         //         return [$item->date => $item->aggregate];
         //     });
@@ -66,13 +66,13 @@ class ChartsPlus
     {
         // Add the last 6 months to the dimensions array
         $orders = Trend::query(Order::completed())
-            ->between(now()->startOfMonth()->subMonths(6), now())
+            ->between(now()->endOfMonth()->subMonths(6), now())
             ->perMonth()->sum('amount')->mapWithKeys(function ($item) {
                 return [$item->date => $item->aggregate];
             });
 
         $transactions = Trend::query(Transaction::status('completed'))
-            ->between(now()->startOfMonth()->subMonths(6), now())
+            ->between(now()->endOfMonth()->subMonths(6), now())
             ->perMonth()->sum('amount')->mapWithKeys(function ($item) {
                 return [$item->date => $item->aggregate];
             });
